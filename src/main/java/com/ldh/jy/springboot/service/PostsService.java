396109bb873a8e1607bdbff434b96cf2374324b6
mttcjd <cbsjhy@gmail.com>
2020-01-09 00:33:18 +0900

    오늘은 여기까즤

--- src/main/java/com/ldh/jy/springboot/service/PostsService.java
+++ src/main/java/com/ldh/jy/springboot/service/PostsService.java
@@ -2,13 +2,16 @@ package com.ldh.jy.springboot.service;
 
 import com.ldh.jy.springboot.domain.posts.Posts;
 import com.ldh.jy.springboot.domain.posts.PostsRepository;
+import com.ldh.jy.springboot.web.dto.PostsListResponseDto;
 import com.ldh.jy.springboot.web.dto.PostsResponseDto;
 import com.ldh.jy.springboot.web.dto.PostsSaveRequestDto;
 import com.ldh.jy.springboot.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
-import javax.transaction.Transactional;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -30,5 +33,12 @@ public class PostsService {
     public PostsResponseDto findById(Long id){
         Posts entity = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 사용자가 없습니다. id="+id));
         return new PostsResponseDto(entity);
+    }
+
+    @Transactional(readOnly = true)//조회 속도를 개선
+    public List<PostsListResponseDto> findAllDesc() {
+        return postsRepository.findAllDesc().stream()
+                .map(PostsListResponseDto::new)
+                .collect(Collectors.toList());
     }
 }
